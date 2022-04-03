6b51e66c2ba23178e6aa0e74d4382e78e419614a
Hornet07 <null>
2022-04-03 15:16:05 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Misak-ı Milli
+// @namespace    http://tampermonkey.net/
+// @version      0.3
+// @description  Misak-ı milli
+// @author       Thanks Jared.
+// @match        https://hot-potato.reddit.com/embed*
+// @updateURL    https://gist.githubusercontent.com/sust4in/372cf01b2c7cbea79c6e69ecf53e6e48/raw/fa6765ab542724bc962b2635d36c597ff16c8c55/misakimilli.js
+// @downloadURL  https://gist.githubusercontent.com/sust4in/372cf01b2c7cbea79c6e69ecf53e6e48/raw/fa6765ab542724bc962b2635d36c597ff16c8c55/misakimilli.js
+// @grant        GM_xmlhttpRequest
+// @connect      raw.githubusercontent.com
+// ==/UserScript==
